6b74f1c2162c7766fc3e22d63e19211c2562094e
Simão Silva <simao.silva@tecnico.ulisboa.pt>
2019-03-21 21:40:18 +0000

    java, tomcat and basic maven configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,35 @@
-# default recipe
+#
+# Cookbook:: fenixedu-cookbook
+# Recipe:: default
+#
+###########################################################
+######################## JAVA #############################
+###########################################################
+node.normal['java'] = {
+  'jdk_version' => '8',
+  'install_flavor' => 'oracle',
+  'set_etc_environment' => true,
+  'oracle' => {
+    'accept_oracle_download_terms' => true,
+    'jce' => {
+      'enabled' => true,
+    },
+  },
+}
+include_recipe 'java'
+
+###########################################################
+###################### TOMCAT #############################
+###########################################################
+include_recipe 'tomcat'
+
+tomcat_install 'fenixedu-webapp' do
+  version '7.0.93'
+end
+
+###########################################################
+###################### MAVEN ##############################
+###########################################################
+
+node.normal['maven']['mavenrc']['opts'] = "#{node['fenixedu-cookbook']['webapp']['java_opts']} -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
+include_recipe 'maven'
